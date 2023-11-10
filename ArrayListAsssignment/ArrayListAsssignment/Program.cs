6d7bacf2dfdac4b7794c16966f914b13dcb6b3b4
VickyBhaskar <vicky2400kumar@gmail.com>
2023-11-10 16:08:15 +0530

    initial Commit

---
+++ ArrayListAsssignment/ArrayListAsssignment/Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections;
+namespace ArrayListAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1: ArrayList Initialization and Population
+            ArrayList studentGrades = new ArrayList();
+            studentGrades.Add(85);
+            studentGrades.Add(92);
+            studentGrades.Add(78);
+            studentGrades.Add(95);
+            studentGrades.Add(88);
+
+            // Task 2: Display and Sum
+            Console.WriteLine("Task 2.1: Display the contents of studentGrades using a loop.");
+            foreach (var grade in studentGrades)
+            {
+                Console.WriteLine(grade);
+            }
+
+            Console.WriteLine("\nTask 2.2: Calculate and display the sum of all the grades in studentGrades.");
+            int sum = 0;
+            foreach (var grade in studentGrades)
+            {
+                sum += (int)grade;
+            }
+            Console.WriteLine("Sum of grades: " + sum);
+
+            // Task 3: Grade Addition
+            Console.WriteLine("\nTask 3.1: Add a new grade of 90 to the end of studentGrades.");
+            studentGrades.Add(90);
+
+            Console.WriteLine("Task 3.2: Display the updated contents of studentGrades after the addition.");
+            foreach (var grade in studentGrades)
+            {
+                Console.WriteLine(grade);
+            }
+
+            // Task 4: Grade Removal
+            Console.WriteLine("\nTask 4.1: Remove the grade of 78 from studentGrades.");
+            studentGrades.Remove(78);
+
+            Console.WriteLine("Task 4.2: Display the contents of studentGrades after the removal.");
+            foreach (var grade in studentGrades)
+            {
+                Console.WriteLine(grade);
+            }
+
+            // Task 5:Search and  Update
+            Console.WriteLine("\nTask 5.1: Search for the index of the grade 95 in studentGrades.");
+            int indexOfGrade95 = studentGrades.IndexOf(95);
+            Console.WriteLine("\nIndex of Grade 95 : \t "+ indexOfGrade95);
+            if (indexOfGrade95 != -1)
+            {
+                Console.WriteLine("Task 5.2: Update the grade at the found index to 96.");
+                studentGrades[indexOfGrade95] = 96;
+            }
+
+            Console.WriteLine("Updated contents of studentGrades:");
+            foreach (var grade in studentGrades)
+            {
+                Console.WriteLine(grade);
+            }
+        }
+    }
+}
+
---
+++ JaggedArray/JaggedArray/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace JaggedArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// Task 1: Declaration and Initialization
+            int[][] matrix = new int[][]
+            {
+            new int[] { 1, 2, 3, 4 },
+            new int[] { 5, 6 },
+            new int[] { 7, 8, 15 }
+            };
+
+            // Task 2: Accessing and Display
+            // Access and store the value of the second element in the first row
+            int element = matrix[0][1];
+
+            Console.WriteLine("Task 2: Display the entire matrix using nested loop");
+            foreach (var row in matrix)
+            {
+                foreach (var item in row)
+                {
+                    Console.Write(item + " ");
+                }
+                // Move to the next line for the next row
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nTask 3: Modify the third element in the second row to have a value of 15");
+            // Modify the third element in the second row
+            if (matrix.Length > 1 && matrix[1].Length > 2)
+            {
+                matrix[1][2] = 15; // Modify the third element in the second row
+            }
+            else
+            {
+                Console.WriteLine("Unable to modify. Insufficient elements in the second row.");
+            }
+
+            // Task 4: Advanced Concepts using dynamic datatype
+            dynamic[][] objectMatrix = new dynamic[][]
+            {
+            new dynamic[] { 10, "Hello", 3.14 },
+            new dynamic[] { true, 'A', DateTime.Now }
+            };
+
+            Console.WriteLine("\nTask 4: Display the contents of the objectMatrix using nested loops");
+            foreach (var row in objectMatrix)
+            {
+                foreach (var item in row)
+                {
+                    Console.Write($"Type: {item.GetType().Name}, Value: {item} | ");
+                }
+                // Move to the next line for the next row
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
